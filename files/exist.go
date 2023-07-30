fe9328a97aebe15616f068f5749ccc68493e0539
K.Yosuke <yksk171118@gmail.com>
2023-07-31 06:04:52 +0900

    :recycle: Remove branch

--- files/exist.go
+++ files/exist.go
@@ -8,17 +8,14 @@ import (
 func ExistFile(path string) (bool, error) {
 	if f, err := os.Stat(path); os.IsNotExist(err) || f.IsDir() {
 		return false, nil
-	} else {
-		return true, nil
 	}
+	return true, nil
 }
 
 // ExistDir check if the directory exists
 func ExistDir(path string) (bool, error) {
 	if f, err := os.Stat(path); os.IsNotExist(err) || !f.IsDir() {
 		return false, nil
-	} else {
-		return true, nil
 	}
-
+	return true, nil
 }
