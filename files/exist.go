95d91bc194b263796f557de58410ed8afe0ec2b9
K.Yosuke <yksk171118@gmail.com>
2023-07-31 05:34:25 +0900

    :sparkles: Add new function to check exist file

--- files/exist.go
+++ files/exist.go
@@ -1,1 +1,13 @@
 package files
+
+import (
+	"os"
+)
+
+func ExistFile(path string) (bool, error) {
+	if f, err := os.Stat(path); os.IsNotExist(err) || f.IsDir() {
+		return false, nil
+	} else {
+		return true, nil
+	}
+}
