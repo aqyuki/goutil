ba9e366338c50dfca6f39379ae4ffaaadc8851ff
K.Yosuke <yksk171118@gmail.com>
2023-07-31 05:47:21 +0900

    :sparkles: Add function to check if the directory exist

--- files/exist.go
+++ files/exist.go
@@ -4,10 +4,21 @@ import (
 	"os"
 )
 
+// ExistFile check if the file exists
 func ExistFile(path string) (bool, error) {
 	if f, err := os.Stat(path); os.IsNotExist(err) || f.IsDir() {
 		return false, nil
 	} else {
 		return true, nil
 	}
+}
+
+// ExistDir check if the directory exists
+func ExistDir(path string) (bool, error) {
+	if f, err := os.Stat(path); os.IsNotExist(err) || !f.IsDir() {
+		return false, nil
+	} else {
+		return true, nil
+	}
+
 }
